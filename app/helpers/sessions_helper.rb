e03876d13f6dc9b363c8531f0923c33c427b1471
EC2 Default User <ec2-user@ip-172-31-27-129.us-east-2.compute.internal>
2018-11-04 00:37:04 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,6 +22,10 @@ module SessionsHelper
     @current_user = nil
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -38,6 +42,13 @@ module SessionsHelper
     !current_user.nil?
   end
   
-  
-  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
